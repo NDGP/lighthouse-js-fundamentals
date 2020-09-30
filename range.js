2fe542c5502236ce5881d4a7cfc2ff725f306276
Nathan <nathanpatrick939@gmail.com>
2020-09-30 17:07:39 +0000

    range.js

--- range.js
+++ range.js
@@ -1,11 +1,13 @@
 let range = (start, end, step) => {
   let arr = []
   for (let i = start; i <= end; i = i + step){
-    arr.push(i)
+  if(step >= 0){ 
+    arr.push(i) 
+  }else{return arr}
   }
   return arr
 };
 
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
+console.log(range(-5, 2, -5));
