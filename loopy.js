9adc59eae3537cdf9b21963901323bc77b394964
Nathan <nathanpatrick939@gmail.com>
2020-09-30 17:41:29 +0000

    loopy.js

--- loopy.js
+++ loopy.js
@@ -1,21 +1,18 @@
-let speachA = "Loopy";
-let speachB = "Lighthouse";
-let speachC = "LoopyLighthouse";
-
-
-for(let counter=100; counter<= 200; counter++){
-  if(counter % 3 === 0 && counter % 4 === 0){
-    console.log(speachC);
+let loopyLighthouse = (range, multiples, words) => {
+for(let counter = range[0]; counter <= range[1]; counter++){
+  if(counter % multiples[0] === 0 && counter % multiples[1] === 0){
+    console.log(words[0] + words [1]);
     continue;
-  }else if(counter % 4 === 0){
-    console.log(speachB);
+  }else if(counter % multiples[0] === 0){
+    console.log(words[0]);
    continue;
- } else if(counter % 3  === 0){
-    console.log(speachA);
+ } else if(counter % multiples[1]  === 0){
+    console.log(words[1]);
    continue;
  }else{
     console.log(counter)
   }
+}
 };
 
-
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
