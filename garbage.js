e6d0103251fb00cd72ca0f4d7b395268d0fae284
Nathan <nathanpatrick939@gmail.com>
2020-10-02 16:46:10 +0000

    garbage.js

---
+++ garbage.js
@@ -0,0 +1,20 @@
+const smartGarbage = function (trash, bins){
+ switch (trash){
+    case "waste":
+      bins.waste = bins.waste + 1;
+      break;
+
+    case "recycling":
+      bins.recycling = bins.recycling + 1;
+      break;
+
+      case "compost":
+    bins.compost = bins.compost + 1;
+    break; 
+ }
+ return bins;
+}
+
+
+
+smartGarbage('waste', { waste: 4, recycling: 2, compost: 5 });
